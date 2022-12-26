88b795b2f656a0d57725bdde96863a2f6d21ed24
Kemo <12032943@mail.sustech.edu.cn>
2022-12-26 12:57:55 +0800

    first commit

---
+++ Monopoly/Assets/UI/Scripts/RnMUI_LoadScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RnMUI_LoadScene : MonoBehaviour {
+
+	public string sceneName;
+	public bool ignoreFirst = true;
+
+	public void LoadScene()
+	{
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
+	}
+
+	public void OnProgress()
+	{
+		if (this.ignoreFirst)
+		{
+			this.ignoreFirst = false;
+			return;
+		}
+		LoadScene();
+	}
+}
